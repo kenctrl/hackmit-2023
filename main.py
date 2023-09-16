ed0d9d84132f23a17c827f92337621e2e7eff3d6
Mark Tabor <3marktabor3@gmail.com>
2023-09-16 19:20:29 -0400

    Changing model and accomodating model I/O parameters

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord import Intents  # Import the Intents class
 import openai
 
 client = discord.Client(intents=Intents.all())  # Specify the intents
-openai.api_key = ""
+openai.api_key = "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"
 
 chatHistory = {}
 
@@ -28,18 +28,22 @@ async def on_message(message):
 
     if message.content == ".reply":
         conversation = "\n".join(chatHistory[channel_id])
-        print("chat history:", chatHistory)
-        print("conversation:", conversation)
+        # print("chat history:", chatHistory)
+        # print("conversation:", conversation)
         prompt = f"{conversation}\nHow would [Your Name] reply: "
         max_tokens = 50
 
-        response = openai.Completion.create(
-          model="text-davinci-003",
-          prompt=prompt,
-          max_tokens=max_tokens
+        response = openai.ChatCompletion.create(  # Use the chat model endpoint
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens = max_tokens,
         )
-        
-        reply = response.choices[0].text.strip()
+        # print(response)
+        reply = response.choices[0].message["content"].strip()
+        await client.user.edit(username=message.author.name)
         await message.channel.send(f"[Mimicking {message.author.name}]: {reply}")
 
-client.run("")
+client.run("MTE1MjczMzMxMzE1MzM4MDQ4Mg.GmtPEq.pue9VY1UYqLa9DRW8RZvQVYx2T0-qfYP6rL4Fs")
